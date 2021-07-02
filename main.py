fc68d7b5114c57d8d76ebe322b80152139e88077
MouinulIslamNJIT <78024242+MouinulIslamNJIT@users.noreply.github.com>
2021-07-01 22:13:09 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from AugGMM.runGMM import runGMM
 from AugMMR.runMMR import runMMR
-from  AugSWAP.runSWAP import runSWAP
+from AugSWAP.runSWAP import runSWAP
+from MaintenanceCodes.GrMn import grMn
+from MaintenanceCodes.NonIncrMn import  nonIncrMn
+from MaintenanceCodes.nonOlMn import nonOlMn
+from MaintenanceCodes.OPTMn import OPTMn
+from MaintenanceCodes.delete import delete
+
 
 def main():
+    # maintenance code
+    # delete()
+    OPTMn()
+    #nonOlMn()
+    #nonIncrMn()
+    #grMn()
+
+
     runGMM(20000,100,1,20)
     runMMR(20000, 100, 1, 20)
     runSWAP(20000, 100, 1, 20)
+
+
+
+
 
 if __name__ == '__main__':
     main()
